01c4cd884b9d1f820080b165ac48fb56cf0d52f2
Hellsing-MO <ghalihellsing123@gmail.com>
2025-07-31 01:20:42 +0300

    he

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,11 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-        protected function schedule(Schedule $schedule): void
-        {
-                $schedule->command('inspire')->everyMinute();
-                $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
-        }
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('inspire')->everyMinute();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
+    }
+
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
