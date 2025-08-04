ef8594403e2e06afd75457d88e1dea3674b65211
Hellsing-MO <ghalihellsing123@gmail.com>
2025-08-05 00:02:06 +0300

    he

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -310,77 +310,8 @@
     <!--features-->
 
     
-<!--contact-->
-  <section class="contact" id="contact">
-      <div class="contact-text">
-        <h2 class="revial-top">{{__('messages.contact us')}}</h2>
-       
-        <div class="social">
-          <a href="https://www.instagram.com/alshahedhoney?igsh=MThka284ZmdsemIxYQ=="><i class='bx bxl-instagram-alt revial-lift'></i></a>
-          <a href="https://www.facebook.com/profile.php?id=61560393087783"><i class='bx bxl-facebook revial-top'></i></a>
-          <a href="#"><i class='bx bxl-twitter revial-bottom'></i></a>
-          <a href="http://wa.me/14379951819/"><i class='bx bxl-whatsapp revial-bottom'></i></a>
-        </div>
-      </div>
+    @extends('home.footer')
 
-      <div class="footer-content">
-        <!-- Quick Links and Contact Info Row -->
-        <div class="footer-row">
-          <!-- Website Links Section -->
-          <div class="footer-section">
-            <h3>Quick Links</h3>
-            <ul class="footer-links">
-              <li><a href="{{url('/')}}"><i class='bx bx-home-alt'></i> {{__('messages.Home')}}</a></li>
-              <li><a href="#about"><i class='bx bx-info-circle'></i> {{__('messages.about us')}}</a></li>
-              <li><a href="{{url('our_shop')}}"><i class='bx bx-store'></i> {{__('messages.our shop')}}</a></li>
-              <li><a href="#contact"><i class='bx bx-phone'></i> {{__('messages.contact us')}}</a></li>
-            </ul>
-          </div>
-
-          <!-- Contact Information Section -->
-          <div class="footer-section">
-            <h3>Contact Info</h3>
-            <div class="contact-details">
-              <div class="contact-item">
-                <a href="https://maps.google.com/?q=45+Grenoble+Dr,+North+York,+ON+M3C+1C4,+Canada" target="_blank">
-                  <i class='bx bxs-location-plus'></i>
-                  <span>45 Grenoble Dr, North York, ON M3C 1C4، Canada</span>
-                </a>
-              </div>
-              <div class="contact-item">
-                <a href="tel:+14379951819">
-                  <i class='bx bx-mobile-alt'></i>
-                  <span>+1 (437) 995-1819</span>
-                </a>
-              </div>
-              <div class="contact-item">
-                <a href="mailto:info@alshahedhoney.com">
-                  <i class='bx bxs-envelope'></i>
-                  <span>info@alshahedhoney.com</span>
-                </a>
-              </div>
-              <div class="contact-item">
-                <a href="https://alshahedhoney.com" target="_blank">
-                  <i class='bx bx-globe'></i>
-                  <span>www.alshahedhoney.com</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Newsletter Section -->
-      </div>
-
-      <!-- Footer Bottom -->
-      <div class="footer-bottom">
-        <div class="container">
-          <p class="copyright">
-            Copyright by <span>Alshahed</span> With <span>LOVE</span>. All rights reserved.
-          </p>
-        </div>
-      </div>
-  </section>
 
     <!--link to js-->
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
